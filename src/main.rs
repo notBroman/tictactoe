4a56fd722f12f70d7c3f1f4af52f4e971c91d269
Roman Berger <romanberger001@gmail.com>
2024-05-04 19:39:48 +0100

    init and displaystarting grid

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,66 @@ pub struct GameBoard {
 
 impl GameBoard{
 
-    pub fn render(&self) {
-        // display the state of the board
+    pub fn init(&mut self) {
+        // initialise the board
+        self.rows = 3;
+        self.cols = 3;
+        self.round = 0;
+
+        for i in 0..self.rows{
+            for j in 0..self.cols{
+                self.board[i][j] = ' ';
+            }
+        }
     }
 
-    pub fn play(&mut self, player: usize, col: usize, row: usize) {
+    fn render(&self) {
+        // display the state of the board
+        for i in 0..self.rows{
+            print!("+-+-+-+\n");
+            print!("|{}|{}|{}|\n", self.board[i][0], self.board[i][1], self.board[i][2]);
+        }
+        print!("+-+-+-+\n");
+    }
+
+    fn play(&mut self, player: usize, c: usize, r: usize) -> bool {
         // place a mark in the spot specified
         // player 1: X
         // player 2: O
+        let symbol;
+        if player == 1 {
+            symbol = 'X';
+        } else {
+            symbol = 'O';
+        }
         
+        self.board[r][c] = symbol;
+        false
+    }
+
+    pub fn game_loop(&mut self){
+        self.render();
+        let mut winner: bool = false;
+        while self.round < 9 && !winner{
+            
+            self.round += 1;
+        }
+
     }
 }
 
+fn setup_board(board: &mut GameBoard) {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = GameBoard {
+        board: [[ ' '; 3]; 3],
+        rows: 3,
+        cols: 3,
+        round: 0,
+    };
+    println!("starting the game!\n\n");
+    
+    game.game_loop();
 }
